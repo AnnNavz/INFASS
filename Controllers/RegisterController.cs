55823dc426f038ece6babc254c2acb47d2c23fd6
AnnNavz <css.annshirleynavasca@gmail.com>
2026-07-17 20:54:38 +0800

    Maytag sakto ni

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using INFASS.Models;
+
+namespace INFASS.Controllers
+{
+    public class RegisterController : Controller
+    {
+        User user = new User();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public IActionResult SubmitRegistration([FromBody] User userData)
+		{
+			if (userData == null)
+			{
+				return BadRequest("No data received.");
+			}
+
+			System.Diagnostics.Debug.WriteLine("================ USER SIGN UP DATA ================");
+			System.Diagnostics.Debug.WriteLine($"Fullname:  {userData.FullName}");
+			System.Diagnostics.Debug.WriteLine($"Username:  {userData.Username}");
+			System.Diagnostics.Debug.WriteLine($"Password:  {userData.Password}");
+			System.Diagnostics.Debug.WriteLine("===================================================");
+
+			string message = $"Successfully received registration for: \n" +
+				$"\n" +
+				$"Fullname: {userData.FullName} \n" +
+				$"Username: {userData.Username} \n" +
+				$"Password: {userData.Password} \n" +
+				$"Confirm Password: {userData.ConfirmPassword}";
+
+			return Json(message);
+		}
+	}
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace INFASS.Models
+{
+	public class User
+	{
+		public string FullName { get; set; } = string.Empty;
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public string ConfirmPassword { get; set; } = string.Empty;
+
+	}
+
+}
